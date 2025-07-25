cde488eba35fe1bc58518d6a9305a2a3fa1c684e
ussefabdelaty <96540998+ussefabdelaty@users.noreply.github.com>
2025-07-25 19:25:25 +0300

    Add files via upload

---
+++ sum_elements_of_array/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace sum_elements_of_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter numbers of array to sum ");
+            string inputNum = Console.ReadLine();
+            int[] numbers = inputNum.Split(',').Select(int.Parse).ToArray();
+            int sum = 0;
+            foreach (var n in numbers)
+            {
+               
+                sum += n;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ sum_elements_of_array/readme.txt
@@ -0,0 +1,10 @@
+🔹Problem:
+Write a program that takes an array of integers and returns the sum of all elements.
+
+🧪 Example:
+
+txt
+Copy
+Edit
+Input: [3, 7, 1, 9]
+Output: 20
